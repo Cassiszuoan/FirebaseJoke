0071a7447a40d84a9553bed9a7822b18b1d24768
Cassiszuoan <jerry831210@gmail.com>
2016-04-25 21:41:56 +0800

    CreateAccountViewController Updated

--- FirebaseJokes/CreateAccountViewController.swift
+++ FirebaseJokes/CreateAccountViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-
+import Firebase
 class CreateAccountViewController: UIViewController {
     
     @IBOutlet weak var usernameField: UITextField!
@@ -26,7 +26,51 @@ class CreateAccountViewController: UIViewController {
     }
     
     @IBAction func createAccount(sender: AnyObject) {
+        let username = usernameField.text
+        let email = emailField.text
+        let password = passwordField.text
         
+        if username != "" && email != "" && password != ""{
+            
+            // Set Email and Password for new Users
+            DataService.dataService.BASE_REF.createUser(email, password: password, withValueCompletionBlock: {error, result in
+                
+                if error != nil {
+                    // There was a problem
+                    self.signupErrorAlert("Oops!", message: "Having some trouble creating your account . Try again later")
+                    
+                }else{
+                    // Create and Login the New User with authUser
+                    DataService.dataService.BASE_REF.authUser(email, password: password, withCompletionBlock:{err, authData in
+                    let user = ["provider": authData.provider!, "email":email!,"username":username!]
+                        
+                        DataService.dataService.createNewAccount(authData.uid,user:user)
+                    
+                    })
+                    
+                    // Store the uid for future access !
+                    NSUserDefaults.standardUserDefaults().setValue(result["uid"], forKey: "uid")
+                    
+                    self.performSegueWithIdentifier("NewUserLoggedIn", sender: nil)
+                }
+                
+                
+    })
+        
+        }
+        
+        else{
+            signupErrorAlert("Oops!", message: "Don't forget to enter your email,password,and a username")
+        }
+        
+    }
+    
+    
+    func signupErrorAlert(title:String , message: String){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+        let action = UIAlertAction(title: "OK", style: .Default, handler: nil)
+        alert.addAction(action)
+        presentViewController(alert,animated:true,completion:nil)
     }
     
     @IBAction func cancelCreateAccount(sender: AnyObject) {
